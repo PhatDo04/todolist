e383a7e6cac5f6f1dc7d7c5d6e52e53a311a44de
PhatDo04 <2251172447@e.tlu.edu.vn>
2025-10-27 17:01:32 +0700

    jquery/xu-ly-html/.add()

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
 $(function(){
-    $('.test').css('color','#ff0000')
-    $('.test').css('border', '1px solid blue')
-
-    $('#hehe').css('color','gray')
-    $('#hehe1').css('color','gray')
-    $('#hehe2').css('color','gray')
-
-    $('.list, #hehe').css('color', 'purple')
-
-    //chon cac thanh phan cu the
-    $('ul li:eq(-3)').css('border', '1px solid black')
-    $('ul li:eq(1)').css('border', '1px solid black')
-    $('ul li:eq(1)').css('border', '1px solid black')
-
-    //chon cac thanh phan lon hon gt(-2)
-    $('ul li:gt(-2)').css('border', '1px solid red')
-
-    //chon cac thanh phan nho hon lt(2)
-    $('ul li:lt(2)').css('border', '5px solid yellow')
-
-    //chon cac thanh phan o vi tri chan :even
-
-    //chon cac thanh phan o vi tri le :odd
-
-    //chon phan tu dau tien :first-child
-
-    //chon phan tu dau tien :last-child
-
-    // chon thanh phan thu 'n' trong thanh phan cha; goc tinh tu thanh phan dau dien 
-    // :nth-child(n)
-    // n co the la so; co the la function
-
-    // [attribute="value"]
-    // giup chon cac thanh phan voi gia tri xac dinh
-    // => lay or gan gia tri thuoc tinh (attribute) cho thanh phan
-
-    
+    //.add(bộ chọn)
+    $('div').add('p').css('color', 'red')
+    //.add(html)
+    $('p').add('<p class = "hehe">tag p co class hehe</p>').appendTo('body');
+    //.add(obj)
+    $('div').add($('p').css('border', '1px solid black')).css('color', 'purple');
+    //.add(bộ chọn, context)
+    $('div').click(function() { 
+        $('div').add('p', this).addClass('test')
+    });
 })
